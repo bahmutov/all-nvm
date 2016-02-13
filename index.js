37dcb4da54dfaab7d42852d158a77b4165878143
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-02-12 23:42:34 -0500

    chore(nvm-api): using patched nvm-api version for now, listed available versions

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const debug = require('debug')('all-nvm')
 const la = require('lazy-ass')
 const is = require('check-more-types')
 const runCommand = require('./src/run-command')
-// const nvmApi = require('nvm-api')
+const nvmApi = require('nvm-api')
 
-function runAll (commandWithOptions) {
-  // debug('loaded NVM api', api)
+function runAll (commandWithOptions, api) {
+  debug('loaded NVM api', api)
+  api.installed(false, function (err, list) {
+    if (err) {
+      throw err
+    }
+    console.log('installed Node versions', list)
+  })
 
   // sh -c '. ~/.nvm/nvm.sh && nvm'
-  const fullNvmCommand = ['sh', '-c', '. ~/.nvm/nvm.sh && nvm exec 4 ' + commandWithOptions.join(' ')]
-  debug('full all nvm command', fullNvmCommand)
+  // const fullNvmCommand = ['sh', '-c', '. ~/.nvm/nvm.sh && nvm exec 4 ' + commandWithOptions.join(' ')]
+  // debug('full all nvm command', fullNvmCommand)
 
-  runCommand(fullNvmCommand).catch(function (error) {
-    console.error(error)
-    process.exit(-1)
-  })
+  // runCommand(fullNvmCommand).catch(function (error) {
+  //   console.error(error)
+  //   process.exit(-1)
+  // })
 }
 
 // command to run NVM in fresh shell
@@ -26,8 +32,8 @@ function allNvm (commandWithOptions) {
   la(is.not.empty(commandWithOptions),
     'missing command, needs at least something', commandWithOptions)
 
-  // return nvmApi.load(runAll.bind(null, commandWithOptions))
-  return runAll(commandWithOptions)
+  return nvmApi.load(runAll.bind(null, commandWithOptions))
+  // return runAll(commandWithOptions)
 }
 
 module.exports = allNvm
