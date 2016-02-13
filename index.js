76e2bb7725932353c2c01be80c75e34faae6343a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-02-12 23:29:35 -0500

    feat(nvm): discovering the correct nvm

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const debug = require('debug')('all-nvm')
 const la = require('lazy-ass')
 const is = require('check-more-types')
 const runCommand = require('./src/run-command')
-const nvmApi = require('nvm-api')
+// const nvmApi = require('nvm-api')
 
-function runAll (commandWithOptions, api) {
-  debug('loaded NVM api', api)
+function runAll (commandWithOptions) {
+  // debug('loaded NVM api', api)
 
-  const fullNvmCommand = ['nvm', 'exec', '4'].concat(commandWithOptions)
-  debug('all nvm', commandWithOptions)
+  // sh -c '. ~/.nvm/nvm.sh && nvm'
+  const fullNvmCommand = ['sh', '-c', '. ~/.nvm/nvm.sh && nvm exec 4 ' + commandWithOptions.join(' ')]
+  debug('full all nvm command', fullNvmCommand)
 
   runCommand(fullNvmCommand).catch(function (error) {
     console.error(error)
@@ -16,13 +17,17 @@ function runAll (commandWithOptions, api) {
   })
 }
 
+// command to run NVM in fresh shell
+// sh -c '. ~/.nvm/nvm.sh && nvm'
+
 function allNvm (commandWithOptions) {
   la(is.array(commandWithOptions),
     'expected command with options', commandWithOptions)
   la(is.not.empty(commandWithOptions),
     'missing command, needs at least something', commandWithOptions)
 
-  return nvmApi.load(runAll.bind(null, commandWithOptions))
+  // return nvmApi.load(runAll.bind(null, commandWithOptions))
+  return runAll(commandWithOptions)
 }
 
 module.exports = allNvm
