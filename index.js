3f872a445194ae124a6207c541b8109c54bfeb63
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-02-13 12:52:50 -0500

    feat(working): working command across multiple node versions

--- index.js
+++ index.js
@@ -2,30 +2,50 @@ const debug = require('debug')('all-nvm')
 const la = require('lazy-ass')
 const is = require('check-more-types')
 const runCommand = require('./src/run-command')
-const nvmApi = require('nvm-api')
+const Promise = require('bluebird')
+const nvmApi = Promise.promisifyAll(require('nvm-api'))
+const hr = require('hr')
+
+/*
+  Note
+  To find nvm command (since it is a script) execute a desired command,
+  for example `nvm --version` like this
+    sh -c '. ~/.nvm/nvm.sh && nvm --version'
+  in particular for using `nvm exec <node version> <command>
+    sh -c '. ~/.nvm/nvm.sh && nvm exec <node version> <command>'
+*/
+function formNvmExec (commandWithOptions, nodeVersion) {
+  la(is.array(commandWithOptions), 'missing command')
+  la(is.unemptyString(nodeVersion), 'missing node version', nodeVersion)
+  return ['sh', '-c', '. ~/.nvm/nvm.sh && nvm exec 4 ' + commandWithOptions.join(' ')]
+}
+
+function runCommandForVersion (command, nodeVersion) {
+  debug('running command for node version', nodeVersion)
+  const commandForNodeVersion = formNvmExec(command, nodeVersion)
+  debug(commandForNodeVersion)
+  hr.hr('=')
+  console.log('Running command on Node', nodeVersion, command)
+  hr.hr('~')
+  return runCommand(commandForNodeVersion)
+}
 
 function runAll (commandWithOptions, api) {
   debug('loaded NVM api', api)
-  api.installed(false, function (err, list) {
-    if (err) {
-      throw err
-    }
-    console.log('installed Node versions', list)
-  })
 
-  // sh -c '. ~/.nvm/nvm.sh && nvm'
-  // const fullNvmCommand = ['sh', '-c', '. ~/.nvm/nvm.sh && nvm exec 4 ' + commandWithOptions.join(' ')]
-  // debug('full all nvm command', fullNvmCommand)
-
-  // runCommand(fullNvmCommand).catch(function (error) {
-  //   console.error(error)
-  //   process.exit(-1)
-  // })
+  return api.installedAsync(false)
+    .tap(function (nodeVersions) {
+      la(is.array(nodeVersions),
+        'expected list of installed node versions', nodeVersions)
+      debug('installed node versions', nodeVersions)
+    })
+    .then(function (nodeVersions) {
+      const runForNode = runCommandForVersion.bind(null, commandWithOptions)
+      return Promise.mapSeries(nodeVersions, runForNode)
+    })
 }
 
-// command to run NVM in fresh shell
-// sh -c '. ~/.nvm/nvm.sh && nvm'
-
+// returns a promise
 function allNvm (commandWithOptions) {
   la(is.array(commandWithOptions),
     'expected command with options', commandWithOptions)
@@ -33,7 +53,6 @@ function allNvm (commandWithOptions) {
     'missing command, needs at least something', commandWithOptions)
 
   return nvmApi.load(runAll.bind(null, commandWithOptions))
-  // return runAll(commandWithOptions)
 }
 
 module.exports = allNvm
