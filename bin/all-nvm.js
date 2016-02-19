81e31c38d8ad8963bde421b3d91b96034a36c744
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-02-19 09:30:17 -0500

    refactor

--- bin/all-nvm.js
+++ bin/all-nvm.js
@@ -8,10 +8,11 @@ const join = require('path').join
 const help =
   'use    : all-nvm <any command> [any options]\n' +
   'example: all-nvm npm i -g manpm'
+const packagePath = join(__dirname, '..', 'package.json')
 
 require('simple-bin-help')({
   minArguments: 4,
-  packagePath: join(__dirname, '..', 'package.json'),
+  packagePath: packagePath,
   help: help
 })
 
